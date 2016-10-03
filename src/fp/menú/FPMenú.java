c1dff459b252365498df4c5e60d942cb28b220ef
alumno <alumno@328-08>
2016-10-03 12:37:44 -0500

    FP-Menu, Muestra de menús consola en JAVA

--- src/fp/menú/FPMenú.java
+++ src/fp/menú/FPMenú.java
@@ -18,15 +18,36 @@ public class FPMenú {
         System.out.println("La opción elegida es: "+ menu());   // TODO code application logic here
     }
     public static int menu (){
-        int opcion;
         Scanner entrada = new Scanner(System.in);
-        System.out.println("****      Cálculo de áreas    ****");
-        System.out.println("**** Menú: ****");
-        System.out.println("Introduzca la opción deseada:");
-        System.out.println("1. Área de cuadrados");
-        System.out.println("2. Área de círculos");
-        System.out.println("3. Área de triángulos");
-        opcion = entrada.nextInt();
+        int opcion;
+        do{
+            System.out.println("****      Cálculo de áreas    ****");
+            System.out.println("**** Menú: ****");
+            System.out.println("Introduzca la opción deseada:");
+            System.out.println("1. Área de cuadrados");
+            System.out.println("2. Área de círculos");
+            System.out.println("3. Área de triángulos");
+            System.out.println("0. Salir");
+            opcion = entrada.nextInt();       
+          }while (opcion>3 || opcion <0);
         return opcion;
+    }
+    
+    public static float calculaAreas (int op){
+        float area;
+        switch (op){
+            case 1: 
+                area = areaCuadrado();
+                break;
+            case 2:
+                area = areaCirculo();
+                break;
+            case 3:
+                area = areaTriangulo();
+                break;
+            default:
+                mensajesalida();
+        }
+        return area;
     }
 }
